b092a7fe0a4fc21fc9d489d3b4c65aff508680e1
Emanuele Brivio <emanuele@plasticpanda.com>
2014-03-26 00:33:06 +0100

    Try with superagent

--- static/js/main.js
+++ static/js/main.js
@@ -14,6 +14,30 @@
     });
   };
 
+  request.get('http://www.kimonolabs.com/api/6mcj643s?apikey=9e9e30f3542ec789be4de05d5c9f16e9', function(response){
+    // Runned when the request is successful
+    console.log(response);
+
+    var container = document.querySelector('#results');
+    
+    response.results.collection1.forEach(function (el) {
+      var item = document.createElement('div')
+        , thumb = document.createElement('img')
+        , name = document.createElement('h4')
+        ;
+      
+      name.textContent = el.name.text;
+      thumb.setAttribute('src', el.thumb.src);
+
+      item.appendChild(thumb);
+      item.appendChild(name);
+      item.classList.add('text-center');
+      item.classList.add('pure-u-1-4');
+
+      container.appendChild(item);
+    });
+  });
+
   qwest.get('http://www.kimonolabs.com/api/6mcj643s?apikey=9e9e30f3542ec789be4de05d5c9f16e9')
     .success(function(response){
       // Runned when the request is successful
